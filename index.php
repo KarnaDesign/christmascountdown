1fbef66669edb6d98a972a1ae722d233e6c62f01
Ida Ferm <ida.kg.ferm@gmail.com>
2016-04-07 17:46:49 +0200

    Bulk commit
    This project wasn't version controlled (for no good reason) so here you
    have it, the whole thing.

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<!-- Background-image licensed through Adobe Stock. -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta description="Keep track on how many days is left until Christmas.">
+        <link type="text/css" rel="stylesheet" href="style/style.css" />
+        <link rel="stylesheet" media="only screen and (max-width: 800px)" href="style/mobile.css" />
+        <script type="text/javascript" src="js/countdown.js"></script>
+        <title>Christmas Countdown</title>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="content">
+                <p id="title" class="reddish">CHRISTMAS COUNTDOWN</p>
+                <p id="countdown" class="reddish">
+                    <span id="days">000</span>
+                    <span>:</span> <!-- to make sure white space is equal on both sides -->
+                    <span id="hours">00</span>
+                    <span>:</span>
+                    <span id="minutes">00</span>
+                    <span>:</span>
+                    <span id="seconds">00</span>
+                </p>
+
+                <!-- Taking the easy road, to avoid creating a database just yet -->
+                <div id="quote1" class="quote">
+                    <p class="text">“One can never have enough socks," said Dumbledore. "Another Christmas has come and gone and I didn't get a single pair. People will insist on giving me books.”</p>
+                    <p class="author">~ J.K. Rowling, Harry Potter and the Sorcerer's Stone ~</p>
+                </div>
+                <div id="quote2" class="quote">
+                    <p class="text">“I will honor Christmas in my heart, and try to keep it all the year.”</p>
+                    <p class="author">~ Charles Dickens ~</p>
+                </div>
+                <div id="quote3" class="quote">
+                    <p class="text">“Christmas, children, is not a date. It is a state of mind.”</p>
+                    <p class="author">~ Mary Ellen Chase ~</p>
+                </div>
+                <div id="quote4" class="quote">
+                    <p class="text">“Christmas waves a magic wand over this world, and behold, everything is softer and more beautiful.”</p>
+                    <p class="author">~ Norman Vincent Peale ~</p>
+                </div>
+                <div id="quote5" class="quote">
+                    <div class="text">
+                    <pre>
+"I heard the bells on Christmas Day
+Their old, familiar carols play,
+And wild and sweet
+The words repeat
+Of peace on earth, good-will to men!”
+                    </pre>
+                    </div>
+                    <p class="author">~ Henry Wadsworth Longfellow ~</p>
+                </div>
+                <div id="quote6" class="quote">
+                    <p class="text">“He who has not Christmas in his heart will never find it under a tree.”</p>
+                    <p class="author">~ Roy L. Smith ~</p>
+                </div>
+                <div id="quote7" class="quote">
+                    <p class="text">“I wish we could put up some of the Christmas spirit in jars and open a jar of it every month.”</p>
+                    <p class="author">~ Harlan Miller ~</p>
+                </div>
+                <div id="quote8" class="quote">
+                    <p class="text">“The perfect Christmas tree? All Christmas trees are perfect!”</p>
+                    <p class="author">~ Charles N. Barnard ~</p>
+                </div>
+                <div id="quote9" class="quote">
+                    <p class="text">“Christmas is never over, unless you want it to be.”</p>
+                    <p class="author">~ Lauren Myracle, Let It Snow ~</p>
+                </div>
+                <div id="quote10" class="quote">
+                    <div class="text">
+                    <pre>
+"At Christmas play and make good cheer,
+For Christmas comes but once a year"
+                    </pre>
+                    </div>
+                    <p class="author">~ Thomas Tusser ~</p>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p>Made by <a class="reddish" href="http://www.karnadesign.se">KarnaDesign</a> © 2015</p>
+        </footer>
+    </body>
+</html>
---
+++ js/countdown.js
@@ -0,0 +1,73 @@
+/**
+ * http://stackoverflow.com/a/1527820
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function showQuote()
+{
+    var randomNumber = getRandomInt(1, 10);
+    var id = "quote" + randomNumber;
+    var quote = document.getElementById(id);
+    quote.style.display = "block";
+}
+
+function addZero(val)
+{
+    if(val < 10)
+    {
+        val = "0" + val;
+    }
+    
+    return val;
+}
+
+function setTime()
+{
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    
+    var now = new Date();
+    var year = now.getFullYear();
+    if(now.getMonth() === 11 && now.getDate() > 25)
+    {
+        year++;
+    }
+    var christmas = new Date(year, 11, 25, 0, 0, 0); //december, 25:e, midnatt
+    
+    var difference = christmas - now; //getting the remaining time between christmas and current (in milliseconds).
+    var remainingDays = Math.floor(difference / day);
+    var remainingHours = Math.floor((difference % day) / hour);
+    var remainingMinutes = Math.floor((difference % hour) / minute);
+    var remainingSeconds = Math.floor((difference % minute) / second);
+    
+   
+    if(remainingDays < 100)
+    {
+        remainingDays = "0" + remainingDays;
+    }
+    
+    remainingDays = addZero(remainingDays);
+    remainingHours = addZero(remainingHours);
+    remainingMinutes = addZero(remainingMinutes);
+    remainingSeconds = addZero(remainingSeconds);
+    
+    document.getElementById("days").textContent = remainingDays;
+    document.getElementById("hours").textContent = remainingHours;
+    document.getElementById("minutes").textContent = remainingMinutes;
+    document.getElementById("seconds").textContent = remainingSeconds;
+    
+    setTimeout(setTime, 1000); //setTimeout=calls a function after a specified amount of time.
+    
+}
+
+window.onload = function()
+{
+    setTime();
+    showQuote();
+}
+
+
